432bf9b0a68208a31013726f497846ca1a8ec05c
Axel Catois - CodingBill <contact@codingbill.com>
2023-03-29 18:55:33 +0200

    add '/' route

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ expressApp.use(morgan("dev"));
 expressApp.use(formidable());
 expressApp.use(cors());
 
+expressApp.get("/", (req, res) => {
+  res.json("Hello World");
+});
+
 expressApp.use(test);
 
 expressApp.listen(process.env.HTTP_PORT || 3310);

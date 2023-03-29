660e19fc470b9c2ba428d780c837295582005a68
Axel Catois - CodingBill <contact@codingbill.com>
2023-03-29 19:24:25 +0200

    trying with yarn

--- src/test.js
+++ src/test.js
@@ -3,9 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.get("/test", (req, res) => {
+  console.log("Triggering the get");
   res.json(req.method);
 });
 router.post("/test", (req, res) => {
+  console.log("Triggering the post");
   res.json(req.method);
 });
 
